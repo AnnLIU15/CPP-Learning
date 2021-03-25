67f47f1c0e44b7710752af8447a5b0349d2d9434
ZhaoY <zqliuzhaoyang@163.com>
2021-03-25 20:32:16 +0800

    加入规则

--- section3/3_1.cpp
+++ section3/3_1.cpp
@@ -2,16 +2,24 @@
  * @Author: ZhaoYang
  * @Date:   2021-03-25 12:49:02
  * @Last Modified by:   ZhaoYang
- * @Last Modified time: 2021-03-25 13:43:27
+ * @Last Modified time: 2021-03-25 20:31:58
  */
 #include <iostream>
 
 using namespace std;
+double inch2foot(double inch);
 
-
-int main(int argc, char const *argv[])
+int main()
 {
+    double inchs;
     /* code */
+    cout<<"___inchs:";
+    cin>>inchs;
+    cout<<inch2foot(inchs);
     return 0;
 }
-
+double inch2foot(double inch)
+{
+    const int trans=12; 
+    return trans*inch;
+}
